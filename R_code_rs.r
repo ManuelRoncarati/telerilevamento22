1a12d5819a20273485c61e4949136e5f44e7d179
ManuelRoncarati <100956737+ManuelRoncarati@users.noreply.github.com>
2022-03-11 16:39:14 +0100

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -1,3 +1,9 @@
 # Questo è il primo script che useremo a lezione
-
+install.packages ("raster")
 library(raster)
+#settaggio cartella di lavoro
+setwd("C:/lab/")
+#caricare i dati, importare i dati
+l2011 <- brick("p224r63_2011.grd")
+#ora voglio ispezionare il file e richiamo l2011
+l2011
